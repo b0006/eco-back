f3ba908ccc3371be2e0fe5f1d1902479f115a807
b0006 <qsefthuken@gmail.com>
2020-09-09 15:54:55 +0700

    fea(auth): init google / get vk user data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,28 @@ const VK_DATA = {
   redirect_uri: 'http://localhost:5000/auth/vk/callback',
 };
 
+interface IVkResponseData {
+  access_token: string;
+  expires_in: number;
+  user_id: number;
+  email?: string;
+}
+
+const GOOGLE_DATA = {
+  client_id: '213999649597-bd74qs4rnaa8sgn1vnb672ivqkfomf5k.apps.googleusercontent.com',
+  client_secret: 'pUyDfNy7HaIFnve-0IxBywN5',
+  redirect_uri: 'http://me.mydomain.com:5000/auth/google/callback',
+  grant_type: 'authorization_code',
+};
+
+interface IGoogleResponseData {
+  access_token: string;
+  expires_in: number;
+  scope: string;
+  token_type: string;
+  id_token: string;
+}
+
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -23,17 +45,50 @@ export class AuthController {
 
   @Get('vk/callback')
   async vkCallback(@Query('code') code: string) {
-    console.log(code);
-    console.log(`https://oauth.vk.com/access_token?${new URLSearchParams({...VK_DATA, code}).toString()}`);
-
     axios.get(`https://oauth.vk.com/access_token?${new URLSearchParams({...VK_DATA, code}).toString()}`)
       .then(response => {
-        console.log(response);
+        const data: IVkResponseData = response.data;
+        console.log(response.data);
+
+        axios.get('https://api.vk.com/method/users.get', {
+          params: { access_token: data.access_token, user_ids: data.user_id, v: '5.122' }
+        })
+          .then(res => {
+            console.log(res.data)
+          })
+          .catch(err => {
+            console.log(err);
+          })
       })
       .catch(error => {
         console.log(error);
       });
 
-    return { status: true };
+    return { status: true, code, type: 'vk' };
   }
+
+  @Get('google/callback')
+  async googleCallback(@Query('code') code: string) {
+    axios.post('https://accounts.google.com/o/oauth2/token', { ...GOOGLE_DATA, code })
+      .then(response => {
+        const data: IGoogleResponseData = response.data;
+        console.log(data);
+
+        axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
+          params: {...GOOGLE_DATA, access_token: data.access_token}
+        })
+          .then(res => {
+            console.log(res)
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    console.log(code);
+    return { status: true, code, type: 'Google' };
+  };
 }
